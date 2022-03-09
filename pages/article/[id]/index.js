6b24b50d75ee0d64d93fbb3443e6a7b8874c53a8
Prateeknandle <prateeknandle@gmail.com>
2022-03-09 20:11:18 +0530

    getStaticPaths

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -14,7 +14,17 @@ const article = ({article}) => {
   )
 }
 //fetch data when req is made
-export const getServerSideProps = async (context)=>{
+// export const getServerSideProps = async (context)=>{
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+//     const article = await res.json()
+//     return{
+//         props: {
+//             article
+//         }
+//     }
+// }
+
+export const getStaticProps = async (context)=>{
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
     const article = await res.json()
     return{
@@ -23,5 +33,17 @@ export const getServerSideProps = async (context)=>{
         }
     }
 }
+
+export const getStaticPaths = async ()=>{
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/`)
+    const articles = await res.json()
+    const ids = articles.map((article) =>article.id)
+    const paths = ids.map((id)=>({params:{id:id.toString()}}))
+    return{
+        paths,
+        fallback:false
+    }
+}
+
 
 export default article
