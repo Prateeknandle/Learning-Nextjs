de8d0a367bb6d33c993e63ddea69a74b8705fea9
Prateeknandle <prateeknandle@gmail.com>
2022-03-09 15:58:14 +0530

    initial basic ui and components

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+const about = () => {
+  return (
+    <div>
+        <Head>
+            <title>About</title>
+        </Head>
+        <h>About</h>
+    </div>
+  )
+}
+
+export default about
